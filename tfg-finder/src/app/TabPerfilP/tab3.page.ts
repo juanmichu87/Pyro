7417c45967d50b46c851e55cc7a176d7d8b6647b
Juan Manuel Moreno <juanmanuelmorenoq@gmail.com>
2024-12-19 18:00:25 +0100

    Acceso al login al cerrar sesion

--- tfg-finder/src/app/TabPerfilP/tab3.page.ts
+++ tfg-finder/src/app/TabPerfilP/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';  // Importar Router
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private router: Router) {}  // Inyectar Router
 
+  cerrarSesion() {
+    this.router.navigate(['/login/login']);  // Redirigir a la página de login
+  }
 }
--- tfg-finder/src/app/login/login.page.ts
+++ tfg-finder/src/app/login/login.page.ts
@@ -18,8 +18,8 @@ export class LoginPage {
   async iniciarSesion() {
     const loading = await this.loadingController.create({
       message: 'Iniciando sesión...',  // Mensaje que aparece en la pantalla de carga
-      spinner: 'circles',  // Tipo de spinner, puedes cambiarlo (por ejemplo, 'dots', 'lines', 'bubbles', etc.)
-      duration: 2000  // Duración del loading (en milisegundos), se puede ajustar o eliminar
+      spinner: 'circular',  // Tipo de spinner, puedes cambiarlo (por ejemplo, 'dots', 'lines', 'bubbles', etc.)
+      duration: 1000  // Duración del loading (en milisegundos), se puede ajustar o eliminar
     });
     await loading.present();  // Mostrar el loading
 
@@ -27,6 +27,6 @@ export class LoginPage {
     setTimeout(() => {
       loading.dismiss();  // Ocultar el loading después del retraso
       this.router.navigate(['/tabs/tab2']);  // Navegar a la vista tab2 después de iniciar sesión
-    }, 2000);  // Simula un retraso de 2 segundos, ajusta según lo necesites
+    }, 1000);  // Simula un retraso de 2 segundos, ajusta según lo necesites
   }
 }
